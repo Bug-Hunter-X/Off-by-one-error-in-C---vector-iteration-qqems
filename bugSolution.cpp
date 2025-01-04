a2e6ed3ec3945f1d28bfa36e189512b943600ed2
BugHunter-X <askzenai@gmail.com>
2025-01-04 08:22:06 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,14 @@
+```cpp
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> vec = {1, 2, 3, 4, 5};
+  for (int i = 0; i < vec.size(); ++i) { //Corrected loop condition
+    cout << vec[i] << endl;
+  }
+  return 0;
+}
+```
